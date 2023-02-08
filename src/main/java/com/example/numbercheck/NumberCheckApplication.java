2421505ee85baef8faa86d4a5f5169ecf3d7994b
Hao_Wei <wei.hao@thoughtworks.com>
2023-02-08 17:36:41 +0800

    init the proj

---
+++ src/main/java/com/example/numbercheck/NumberCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.numbercheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberCheckApplication.class, args);
+	}
+
+}
